b3e64ad50f0eeabe6b26187006ccb5fe0e132497
Sanjit <iamsanjit@hotmail.com>
2018-02-10 00:16:32 -0500

    Adding database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Thread;
+use App\Reply;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $threads = factory(Thread::class, 50)->create();
+        
+        foreach ($threads as $thread) {
+            factory(Reply::class, 10)->create(['thread_id' => $thread->id]);
+        }
+
+        factory(User::class, 1)->create([
+            'name' => 'Jane Doe',
+            'email' => 'jane.doe@example.com',
+            'password' => bcrypt('password')
+        ]);
     }
 }
