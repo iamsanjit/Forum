812ffb5a9f0c008a110f7835d56e5947143b0798
Sanjit <iamsanjit@hotmail.com>
2018-02-10 21:40:00 -0500

    Create new thread validation errors and old data

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -10,20 +10,44 @@
                 </div>
                 <div class="panel-body">
                     <form method="POST" action="{{ route('threads.store') }}">
+
                         {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <label for="title">Select channel</label>
+                            <select class="form-control" name="channel_id" id="channel_id">
+                                <option value="">Choose one ..</option>
+                                @foreach(App\Channel::all() as $channel)
+                                    <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : ''}}>{{ $channel->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
                         <div class="form-group">
                             <label for="title">Title</label>
-                            <input type="text" name="title" class="form-control">
+                            <input type="text" name="title" class="form-control" value="{{ old('title') }}">
                         </div>
+
                         <div class="form-group">
                             <label for="body">Body</label>
-                            <textarea type="text" name="body" class="form-control" rows="5"></textarea>
+                            <textarea type="text" name="body" class="form-control" rows="5">{{ old('body') }}</textarea>
                         </div>
-                        <input type="submit" value="Create">
+
+                        <div class="form-group">
+                            <input type="submit" value="Create">
+                        </div>
+
+                        @if(count($errors))
+                            <ul class="alert alert-danger">
+                                @foreach($errors->all() as $error)
+                                    <li>{{$error}}</li>
+                                @endforeach
+                            </ul>
+                        @endif
                     </form>
                 </div>
             </div>
-        </div>
+        </div>  
     </div>
 </div>
 @endsection
