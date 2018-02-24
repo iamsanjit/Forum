778a9e3b3be41e3f732838f0aed14342a6e2f9a8
Sanjit <iamsanjit@hotmail.com>
2018-02-24 18:02:29 -0500

    Add flexible way to record certain activities.

--- app/RecordActivity.php
+++ app/RecordActivity.php
@@ -8,19 +8,27 @@ trait RecordActivity
 {
     protected static function bootRecordActivity()
     {
-        self::created(function ($thread) {
-            $thread->recordActivity('created');
-        });
+        foreach (self::getActivitiesToRecord() as $event) {
+            self::$event(function ($model) use ($event) {
+                $model->recordActivity($event);
+            });
+        }
+    }
+
+    protected static function getActivitiesToRecord()
+    {
+        return ['created'];
     }
 
     protected function recordActivity($event)
     {
-        if (auth()->check()) {
-            $this->activity()->create([
-                'type' =>  $this->getActivityType($event),
-                'user_id' => auth()->id(),
-            ]);
+        if (auth()->guest()) {
+            return;
         }
+        $this->activity()->create([
+            'type' =>  $this->getActivityType($event),
+            'user_id' => auth()->id(),
+        ]);
     }
 
     protected function getActivityType($event)
